cf9d3029e38d8c86e81d6d3e75bb57869e198e78
Waleed ur Rehman <waleedrehman31@gmail.com>
2021-07-24 14:35:04 +0500

    First commit

---
+++ playground/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# request -> response
+# request handler
+# action
+
+
+def say_hello(request):
+    return render(request, 'index.html', {'name': 'Waleed ur Rehman'})
